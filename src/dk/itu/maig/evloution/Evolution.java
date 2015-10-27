9b7e393de76b1decda9915810c7ae68c0e509963
AaronGitH <agor@itu.dk>
2015-10-27 14:18:41 +0100

    A first glimpse of evolution

---
+++ src/dk/itu/maig/evloution/Evolution.java
@@ -0,0 +1,27 @@
+package dk.itu.maig.evloution;
+
+/**
+ * @author Aaron Gornott <agor@itu.dk>
+ */
+public class Evolution {
+	
+	// EVOLVE_GENETIC_CONTROLLER
+	public static void main(String args[]) {
+		int POPULATION_SIZE = 200;
+		int ELITE_SIZE = 5; // elitism of best individuals
+
+		// generate initial population
+
+		int generationCount = 0;
+		while (true) { // user decides when to stop
+
+			// evaluate generation
+
+			// produce next generation
+
+			//population = nextGeneration;
+            generationCount++;
+		}
+	}
+
+}
