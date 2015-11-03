726b19112ec819fcdc0972513bd423929f485aca
AaronGitH <agor@itu.dk>
2015-11-03 19:28:12 +0100

    speed up static world evolution

--- src/dk/itu/maig/evloution/RunEvolution.java
+++ src/dk/itu/maig/evloution/RunEvolution.java
@@ -15,7 +15,8 @@ public class RunEvolution {
 	
 	static final int POPULATION_SIZE = 200;
 	static final int ELITE_SIZE = POPULATION_SIZE / 40; // elitism of best individuals
-	static final int REPRODUCTION_TOP = POPULATION_SIZE / 10; // reproduction count for best individual
+	static final int REPRODUCTION_TOP = POPULATION_SIZE / 11; // reproduction count for best individual (next best x--; until x==1)
+	static final int NUM_EVAL_TRIALS=1; // TODO:  = 10; // run count for avg evaluation
 	
 	// EVOLVE_GENETIC_CONTROLLER
 	public static void main(String args[]) throws Exception {
@@ -30,16 +31,15 @@ public class RunEvolution {
 		int generationCount = 0;
 		while (true) { // user decides when to stop
 			
-			// evaluate generation			
-			int numTrials=10;
+			// evaluate generation
             for(int i=0; i<POPULATION_SIZE; i++){
             	MAIGSimulator sim = new MAIGSimulator(false, 0, 1); //TODO level seed is still static?
             	population.get(i).setPhenotype(new PhenotypeMario(sim));
             	double score = 0;
-            	for(int n=0; n < numTrials; n++){
+            	for(int n=0; n < NUM_EVAL_TRIALS; n++){
             		score += sim.simulate(new NN(8,7,6, population.get(i).getNewtworkWeights() ));
             	}
-            	score = score / numTrials;
+            	score = score / NUM_EVAL_TRIALS;
             	//System.out.println("phenotype["+ i +"].avgFitness: " + score);
             	population.get(i).getPhenotype().setFitness(score);
             }
